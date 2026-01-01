ca8412984e7c54564190e58aa16493f7ee9df2f1
Amin <ayari.am@campus.lmu.de>
2026-01-01 16:22:54 +0100

    added icons, working local storage

--- Data/script.js
+++ Data/script.js
@@ -1,33 +1,35 @@
-const gratefulNotes = [];
+var gratefulNotes = [];
 var typed_content ='';
 
 gratefulNotes[0] = { date: new Date().toLocaleDateString(), note: "Listening to my favorite song" }
 gratefulNotes[1] = { date: new Date().toLocaleDateString(), note: "The quiet morning to enjoy a cup of coffee" }
 gratefulNotes[2] = { date: new Date().toLocaleDateString(), note: "My dog greeting me every time I come home" }
 gratefulNotes[3] = { date: new Date().toLocaleDateString(), note: "A peaceful walk outside to clear my mind" }
-gratefulNotes[4] = { date: new Date().toLocaleDateString(), note: "Finishing a tough project" }
-gratefulNotes[5] = { date: new Date().toLocaleDateString(), note: "A beautiful sunset" }
+// gratefulNotes[4] = { date: new Date().toLocaleDateString(), note: "Finishing a tough project" }
+// gratefulNotes[5] = { date: new Date().toLocaleDateString(), note: "A beautiful sunset" }
 
 //todo dynamically fill and store table from entries.txt to better database format
-
+// localStorage.setItem('gratefulNotesSaved',JSON.stringify(gratefulNotes)) ; //save to local storage
+var savedArray =[]
 const saved = localStorage.getItem('gratefulNotesSaved')
 if (saved){
-    // gratefulNotes.push( JSON.parse(saved));
-    // gratefulNotes = JSON.parse(saved);
-    // gratefulNotes.forEach(updateUI);
-}
+    savedArray= JSON.parse(saved);
+} 
 
 
 
 //populating html container
 const nc = document.getElementById('notes-container');
 
-gratefulNotes.forEach(updateUI);
+savedArray.forEach(updateUI);
 
 function updateUI(element){
     nc.innerHTML += `<div class="note-card">
             <p>${element.note}</p>
-        <small class="date">${element.date}</small>
+        <small class="date">${element.date}        
+         <img src="Data/recycle-bin.png">
+         <img src="Data/edit.png">
+</small>
     </div>
    `;
 
@@ -42,8 +44,8 @@ document.getElementById('note-content').addEventListener('keydown',function (e){
 function nnFn(e){
     // nc.innerHTML += `add ${typed_content}`;
     const new_note_card = { date: new Date().toLocaleDateString(), note: typed_content };
-    gratefulNotes.push( new_note_card)
-    // localStorage.setItem('gratefulNotesSaved',JSON.stringify(gratefulNotes)) ; //save to local storage
+    savedArray.push( new_note_card);
+    localStorage.setItem('gratefulNotesSaved',JSON.stringify(savedArray)) ; //save to local storage
     updateUI(new_note_card);
     e.target.value=''; //empties the input field 
     // todo empty when clicked on '+' button
