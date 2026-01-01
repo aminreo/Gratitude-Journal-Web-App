548c66042f7f54b85b1d1f522b6ec5affd2426a6
Amin <ayari.am@campus.lmu.de>
2026-01-01 13:16:52 +0100

    added real date strings

--- Data/script.js
+++ Data/script.js
@@ -1,12 +1,12 @@
 const gratefulNotes = [];
 var typed_content ='';
 
-gratefulNotes[0] = { date: "123", note: "listening to my favorite song" }
-gratefulNotes[1] = { date: "124", note: "The quiet morning to enjoy a cup of coffee before the day starts" }
-gratefulNotes[2] = { date: "125", note: "My dog greeting me every time I come home" }
-gratefulNotes[3] = { date: "126", note: "A peaceful walk outside to clear my mind" }
-gratefulNotes[4] = { date: "127", note: "Finishing a tough project" }
-gratefulNotes[5] = { date: "128", note: "A beautiful sunset" }
+gratefulNotes[0] = { date: new Date().toLocaleDateString(), note: "Listening to my favorite song" }
+gratefulNotes[1] = { date: new Date().toLocaleDateString(), note: "The quiet morning to enjoy a cup of coffee before the day starts" }
+gratefulNotes[2] = { date: new Date().toLocaleDateString(), note: "My dog greeting me every time I come home" }
+gratefulNotes[3] = { date: new Date().toLocaleDateString(), note: "A peaceful walk outside to clear my mind" }
+gratefulNotes[4] = { date: new Date().toLocaleDateString(), note: "Finishing a tough project" }
+gratefulNotes[5] = { date: new Date().toLocaleDateString(), note: "A beautiful sunset" }
 
 //todo dynamically fill and store table from entries.txt to better database format
 const nc = document.getElementById('notes-container');
@@ -30,7 +30,7 @@ document.getElementById('note-cotent').addEventListener('keydown',function (e){i
 
 function nnFn(e){
     // nc.innerHTML += `add ${typed_content}`;
-    const new_note_card = { date: "129", note: typed_content };
+    const new_note_card = { date: new Date().toLocaleDateString(), note: typed_content };
     gratefulNotes.push( new_note_card)
     updateUI(new_note_card);
     e.target.value=''; //empties the input field 
